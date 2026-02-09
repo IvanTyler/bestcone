e232ad800a6bb60e11ea87a484af755959d429c8
Ivan <Ivan_567.92@mail.ru>
2026-02-09 22:14:30 +0300

    edit cors

--- server/app.js
+++ server/app.js
@@ -9,11 +9,20 @@ const organizationsInfoRouter = require('./routes/organizationInfoRouter');
 
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3005 || 10000;
 
 app.use(cors({
-    origin: ['https://bestcone-api.onrender.com/'], // все возможные порты
-    credentials: true
+    origin: [
+        'https://bestcone.vercel.app',    // Продакшн
+        'http://172.17.112.1:3000',       // Vite с IP
+        'http://localhost:3000',          // Локальный Vite
+        'http://127.0.0.1:3000',          // Альтернативный локальный
+        'http://localhost:5173'           // Vite по умолчанию
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+    optionsSuccessStatus: 200
 }));
 
 app.use(express.json());
--- server/routes/organizationInfoRouter.js
+++ server/routes/organizationInfoRouter.js
@@ -6,6 +6,8 @@ const {organizations} = require("../data");
 router.post('/', (req, res) => {
     const { id } = req.body
 
+    console.log('id>>>', id)
+
     const organizationInfo = organizations.find(el => el.id === id);
     res.json(organizationInfo)
 });
